f6373b3b8467c4810ff112dbee6a2d553435d89e
John Elison Duero <itglobalhome24@gmail.com>
2024-09-26 16:52:43 +0800

    Edited the sample product data, edited product details, edited cart page.
    Removed quantity. Added member toggle button on product details.

--- e-commerce-app/src/pages/cart/index.jsx
+++ e-commerce-app/src/pages/cart/index.jsx
@@ -26,11 +26,8 @@ const Cart = (props) => {
             price: 800,
             category: 'Accident Insurance',
             description: 'Accident insurance is a type of insurance plan that pays out when someone is hurt or killed in an accident. This kind of insurance typically excludes coverage for carelessness, divine acts, and natural disasters. The policy may also have restrictions, such as caps on total payouts or prohibitions on payouts for risky behaviours. A lot of insurance providers offer accident insurance, which may be bought separately or added to an existing policy.',
-            images: ['https://res.cloudinary.com/silviajcn/image/upload/v1642282411/SPRING-3/productos/laptop5/laptop5.1_bet4br.jpg', 'https://res.cloudinary.com/silviajcn/image/upload/v1642282413/SPRING-3/productos/laptop5/laptop5.2_p2dfco.jpg', 'https://res.cloudinary.com/silviajcn/image/upload/v1642282416/SPRING-3/productos/laptop5/laptop5.3_hmnoaf.jpg'],
-            banner: 'https://res.cloudinary.com/silviajcn/image/upload/v1642282419/SPRING-3/productos/laptop5/laptop5.4_xsacd8.jpg',
-            brand: '',
-            rate: 3,
-            quantity: 11
+            images: ["/src/assets/images/accident_insurance_icon.jpeg"],
+            coverage: ["Accidental Disablement and Dismemberment", "Accidental Medical Reimbursement", "Daily Hospital Confinement Benefit due to Accident (max of 15 days)"]
         }
         addToCart(suggestedItem)
         setSuggestionAdded(true)
@@ -47,17 +44,17 @@ const Cart = (props) => {
                     <h4 className="text-gray-800 text-lg mb-4 font-medium">ORDER SUMMARY</h4>
                     <ul>
                         <li className="flex border-b border-gray-200">
-                            <h5 className="text-gray-800 font-semibold w-1/2">Product</h5>
-                            <p className="text-gray-800 font-semibold text-center w-20">Quantity</p>
+                            <h5 className="text-gray-800 font-semibold w-20">Product</h5>
+                            <span className='w-1/2'></span>
                             <p className="text-gray-800 font-semibold text-center w-24">Price</p>
                             <span className='w-20'></span>
                         </li>
                         {cartProducts.map((data, index) => (
                             <li key={index} className="flex border-b border-gray-200 py-4 mb-4">
-                                <h5 className="text-gray-800 font-medium w-1/2 pt-1">{data.title}</h5>
-                                <p className="text-gray-600 w-20 text-center px-3 pt-1">{data.quantity}</p>
-                                <p className="text-gray-800 font-medium w-24 text-center px-3 pt-1">₱ {data.price}</p>
-                                <button onClick={() => handleClickRemove(data)} className='bg-primary text-white first-letter:first-line:w-20 h-1/3 rounded-md p-1'>Remove</button>
+                                <img src={data.images[0]} alt="" className='h-16 w-20' />
+                                <h5 className="text-gray-800 font-medium w-1/2 pt-4">{data.title}</h5>
+                                <p className="text-gray-800 font-medium w-24 text-center px-3 pt-4">₱ {data.price}</p>
+                                <button onClick={() => handleClickRemove(data)} className='bg-primary text-white first-letter:first-line:w-20 h-1/3 rounded-md p-1 mt-3'>Remove</button>
                             </li>
                         ))}
                     </ul>
